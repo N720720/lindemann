987dd5a55518bd969b00b445227fccf59c47000f
Sebastian Thurm <sebastian.thurm@stud.uni-goettingen.de>
2020-06-25 00:55:15 +0200

    added functionality to main

--- lindemann/__main__.py
+++ lindemann/__main__.py
@@ -4,11 +4,14 @@ import random
 from enum import Enum
 from typing import Optional
 
+import numpy as np
 import typer
 from rich.console import Console
 
 from lindemann import __version__
 from lindemann.example import hello
+from lindemann.index.per_trj import lindemann_process_frames
+from lindemann.trjread import frames
 
 
 class Color(str, Enum):
@@ -39,14 +42,16 @@ def version_callback(value: bool):
 
 @app.command(name="")
 def main(
-    name: str = typer.Option(..., help="Name of person to greet."),
+    trjfile: str = typer.Option(
+        None, "-t", "--traj", help="The Trajectory file you want to process.",
+    ),
     color: Optional[Color] = typer.Option(
         None,
         "-c",
         "--color",
         "--colour",
         case_sensitive=False,
-        help="Color for name. If not specified then choice will be random.",
+        help="Color for trjfile. If not specified then choice will be random.",
     ),
     version: bool = typer.Option(
         None,
@@ -56,11 +61,25 @@ def main(
         is_eager=True,
         help="Prints the version of the lindemann package.",
     ),
+    index: bool = typer.Option(
+        None,
+        "-i",
+        "--index",
+        help="Calculates the lindemann index for the given lammps trajectory.",
+    ),
 ):
-    """Prints a greeting for a giving name."""
+    """Prints a greeting for a giving trjfile."""
     if color is None:
         # If no color specified use random value from `Color` class
         color = random.choice(list(Color.__members__.values()))
-
-    greeting: str = hello(name)
-    console.print(f"[bold {color}]{greeting}[/]")
+    trjfile: str = trjfile
+    console.print(trjfile)
+    my_frames = frames.frames(trjfile)
+    indices = lindemann_process_frames(
+        my_frames, len(my_frames), len(my_frames)
+    )
+    ind = np.mean(np.nanmean(indices, axis=1))
+    console.print(ind)
+    # index = lindemann.index.per_trj.lindemann_process_frames(trjfile)
+    # greeting: float = index
+    # console.print(f"The lindemann index is [bold {color}]{greeting}[/]")
