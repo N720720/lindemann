95430a2ac4de416601def120ce32f976779f7686
N720720 <s.thurm@protonmail.com>
2022-01-19 16:35:15 +0100

    reduced the precision further

--- tests/test_example/test_cli.py
+++ tests/test_example/test_cli.py
@@ -38,7 +38,7 @@ def test_a_p_flags():
 
 def test_t_flag():
     flag = "-t"
-    res_str = "lindemann index for the Trajectory: 0.02642670983"
+    res_str = "lindemann index for the Trajectory: 0.0264267"
     trajectory = ["tests/test_example/459_02.lammpstrj"]
     single_process_and_multiprocess(trajectory, flag, res_str)
 
@@ -53,7 +53,7 @@ def test_m_flag():
 def test_none_flag():
     result = runner.invoke(app, ["tests/test_example/459_02.lammpstrj"])
     assert result.exit_code == 0
-    assert "lindemann index for the Trajectory: 0.02642670983" in result.stdout
+    assert "lindemann index for the Trajectory: 0.0264267" in result.stdout
 
 
 def test_none_flag_multi():
@@ -61,5 +61,5 @@ def test_none_flag_multi():
         app, ["tests/test_example/459_01.lammpstrj", "tests/test_example/459_02.lammpstrj"]
     )
     assert result.exit_code == 0
-    assert "0.025923892566" in result.stdout
-    assert "0.02642670983" in result.stdout
+    assert "0.0259238" in result.stdout
+    assert "0.0264267" in result.stdout
