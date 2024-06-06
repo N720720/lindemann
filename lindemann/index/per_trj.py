697773dd31f522a177eb3acb439f67a10e324931
Sebastian Thurm <sebastian.thurm@stud.uni-goettingen.de>
2024-06-06 18:17:39 +0200

    #467 #473 Use of distance vector and the combined loop

--- lindemann/index/per_trj.py
+++ lindemann/index/per_trj.py
@@ -1,64 +1,29 @@
-from typing import Any
-
 import numba as nb
 import numpy as np
-import numpy.typing as npt
-
-# No typing for jit functions https://github.com/numba/numba/issues/7424
 
 
-@nb.njit(fastmath=True, error_model="numpy")  # type: ignore
-def lindemann_per_atom(frames: npt.NDArray[np.float32]) -> Any:
-    """Calculate the lindeman index
-    Args:
-        frames: numpy array of shape(frames,atoms)
-    Returns:
-        float32: returns the lindeman index
-    """
+@nb.njit(fastmath=True)
+def calculate(positions):
+    num_frames, num_atoms, _ = positions.shape
+    num_distances = num_atoms * (num_atoms - 1) // 2
 
-    dt = frames.dtype
-    natoms = len(frames[0])
-    nframes = len(frames)
-    array_mean = np.zeros((natoms, natoms), dtype=dt)
-    array_var = np.zeros((natoms, natoms), dtype=dt)
-    iframe = dt.type(1)
-    for coords in frames:
+    mean_distances = np.zeros(num_distances, dtype=np.float32)
+    m2_distances = np.zeros(num_distances, dtype=np.float32)
 
-        # here we do someting similar to scipy's spatial.distance.pdist scipy.spatial.distance.pdist
-        n, p = coords.shape
-        array_distance = np.zeros((n, n), dtype=dt)
-        for i in range(n):
-            for j in range(i + 1, n):
-                d = dt.type(0.0)
-                for k in range(p):
-                    d += (coords[i, k] - coords[j, k]) ** dt.type(2)
-                array_distance[i, j] = np.sqrt(d)
-        array_distance += array_distance.T
+    for frame in range(num_frames):
+        index = 0
+        frame_count = frame + 1
+        for i in range(num_atoms):
+            for j in range(i + 1, num_atoms):
+                dist = 0.0
+                for k in range(3):
+                    dist += (positions[frame, i, k] - positions[frame, j, k]) ** 2
+                dist = np.sqrt(dist)
+                delta = dist - mean_distances[index]
+                mean_distances[index] += delta / frame_count
+                delta2 = dist - mean_distances[index]
+                m2_distances[index] += delta * delta2
 
-        #################################################################################
-        # update mean and var arrays based on Welford algorithm suggested by Donald Knuth
-        #################################################################################
-        for i in range(natoms):
-            for j in range(i + 1, natoms):
-                xn = array_distance[i, j]
-                mean = array_mean[i, j]
-                var = array_var[i, j]
-                delta = xn - mean
-                array_mean[i, j] = mean + delta / iframe
-                array_var[i, j] = var + delta * (xn - array_mean[i, j])
-        iframe += 1.0  # type: ignore[assignment]
-        if iframe > nframes:
-            break
+                index += 1
 
-    for i in range(natoms):
-        for j in range(i + 1, natoms):
-            array_mean[j, i] = array_mean[i, j]
-            array_var[j, i] = array_var[i, j]
-
-    lindemann_indices = np.divide(np.sqrt(np.divide(array_var, nframes)), array_mean)
-    return lindemann_indices
-
-
-def calculate(frames: npt.NDArray[np.float64]) -> float:
-
-    return np.mean(np.nanmean(lindemann_per_atom(frames), axis=1))  # type: ignore[no-any-return, no-untyped-call]
+    return np.mean(np.sqrt(m2_distances / num_frames) / mean_distances)
